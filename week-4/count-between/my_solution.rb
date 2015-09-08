a5eda3719da22f91cbac5ff9d0257e41b7a071b1
mikecerrone <mikecerrone@gmail.com>
2015-09-08 10:00:29 -0400

    add 4.6.6

--- week-4/count-between/my_solution.rb
+++ week-4/count-between/my_solution.rb
@@ -1,6 +1,6 @@
 # Count Between
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: stack overflow].
 
 # count_between is a method with three arguments:
 #   1. An array of integers
@@ -15,5 +15,6 @@
 # Your Solution Below
 
 def count_between(list_of_integers, lower_bound, upper_bound)
-  # Your code goes here!
+ return 0 if lower_bound > upper_bound
+  list_of_integers.count{|v| (lower_bound..upper_bound).include?(v)}
 end
