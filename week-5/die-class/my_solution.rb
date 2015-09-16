653abede256adaa7a663c7145797cf6da5c0dd97
mikecerrone <mikecerrone@gmail.com>
2015-09-16 17:35:56 -0400

    add dice

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -16,17 +16,26 @@
 class Die
   def initialize(sides)
     # code goes here
+  @sides1 = sides
+  unless sides > 0
+    raise ArgumentError.new("Only positive numbers are allowed ;) ")
+  end
+  # ... Do the actual work
+    roll
   end
 
   def sides
-    # code goes here
-  end
+    return @sides1
+end
 
   def roll
-    # code goes here
+    roller = 1 + rand(@sides1)
   end
 end
 
+die = Die.new(0)
+puts die.sides
+puts die.roll
 
 
 # 3. Refactored Solution
