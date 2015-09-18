4ca51d47b04d56781d01d35b01e0d342809440d9
mikecerrone <mikecerrone@gmail.com>
2015-09-17 18:22:53 -0700

    add die

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -55,10 +55,20 @@ puts die.roll
 
 
 # What is an ArgumentError and why would you use one?
+    # - an arguement error represents an error that occurs when the arguments supplied in a function do not match the arguments defined for that function. You would use one if a method needed to pass 2 arguements and only one was passed.
 # What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
+    # - initialize, it was new since it was the first time that we have worked with classes. It took me a few tries to figure out how to use it.
+
 # What is a Ruby class?
+
+    # - A ruby class is an object that is created and has its own methods that can be run on it.
+
 # Why would you use a Ruby class?
+    # - to keep methods that are used together in one place and that use similar variables
+
 # What is the difference between a local variable and an instance variable?
+    # - A local variable can only be used within the method where an instance variable can be used throughout the instance of the class.
 # Where can an instance variable be used?
+    # - An instance variable can be used in multiple methods within the instance of the class.
 
 
