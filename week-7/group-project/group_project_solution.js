b641dc62830cbbc8aa8ba1815fcfe58e86187c0c
mikecerrone <mikecerrone@gmail.com>
2015-09-29 12:29:58 -0700

    groupp2

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,14 +1,14 @@
 
 // User stories //
 
-  // 1. As a user I would want to create a function called sum. This function takes an array as an argument.
-  // 2. The function sum should return the sum of all elements in an array with an odd length.
-  // 3. The function sum should return the sum of all elements in an array with an even length.
+  // 1. As a user I would want to create a function called sum. This function takes an organized list  of numbers as an argument.
+  // 2. The function sum should return the sum of all elements in the number group with an odd length.
+  // 3. The function sum should return the sum of all elements in the number group with an even length.
 
   // 4. As a user I would want to create a function called mean. This function takes an array as an argument.
-  // 5. Mean should return the average of all elements in an array with an odd length.
-  // 6. Mean should return the average of all elements in an array with an even length.
+  // 5. Mean should return the average of all elements in the number group with an odd length.
+  // 6. Mean should return the average of all elements in the number group with an even length.
 
-  // 7. As a user I would want to create a function called median. This function takes an array as an argument.
-  // 8. median should return the median value of all elements in an array with an odd length.
-  // 9. median should return the median value of all elements in an array with an even length.
+  // 7. As a user I would want to create a function called median. This function takes an organized list of numbers as an argument.
+  // 8. median should return the median value of all elements in the number group with an odd length.
+  // 9. median should return the median value of all elements in the number group with an even length.
