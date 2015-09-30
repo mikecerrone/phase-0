c81e8ed623266ea6d3b7736d68088144f6ee0486
mikecerrone <mikecerrone@gmail.com>
2015-09-30 09:41:25 -0700

    ps 1

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -62,3 +62,13 @@ console.log(seperateComma(50000));
 
 
 // Reflection
+
+
+// What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+  //-- Yes, I found it much harder since there do not see to be as many built in methods as ruby.
+// What did you learn about iterating over arrays in JavaScript?
+  //-- It is more difficult an seems like the language is not built to iterate. It feels like we were hacking it together.
+// What was different about solving this problem in JavaScript?
+  //-- At least in our solution, it was much more complicated. I am not sure if we did it a very round about way but we had a ton of difficulty.
+// What built-in methods did you find to incorporate in your refactored solution?
+ //-- We used .split, .reverse, .toSrting and .join
